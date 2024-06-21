3c7123e88e8a79058b129bec2a048b31a05b59f4
Adrian Pantea <adrianpantea10@gmail.com>
2024-06-21 19:41:17 +0100

    analytics changed

--- app/layout.js
+++ app/layout.js
@@ -45,22 +45,7 @@ export default function RootLayout({ children }) {
       <body
         className={`mx-auto max-w-xl px-5 bg-lightBackground dark:bg-background min-h-screen `}
       >
-        {GA_ID && (
-          <>
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-              strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
-              {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${GA_ID}');
-              `}
-            </Script>
-          </>
-        )}
+        <GoogleAnalytics gaId={GA_ID} />
         {children}
         <Footer />
       </body>
