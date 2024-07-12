5e3b02395ab65855c8b12f71cb4ee36eb9a77628
Adrian Pantea <adrianpantea10@gmail.com>
2024-07-12 23:35:28 +0100

    changed title fonts and added google maps reference in contact page

--- app/layout.js
+++ app/layout.js
@@ -22,10 +22,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
       <Script
-        src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_MAPS_API_KEY}`}
-        strategy="beforeInteractive"
-      />
-      <Script
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
@@ -40,7 +36,6 @@ export default function RootLayout({ children }) {
         });
       `}
       </Script>
-
       <head>
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="cat.png" sizes="60x60" type="image/png" />
